73c638b9430d7f442927cab634cdcb3764280870
jiangwenping <jwpttcg66>
2021-09-07 15:22:24 +0800

    Initial commit

---
+++ src/test/java/com/snowcattle/game/hbasedemo/HbasedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowcattle.game.hbasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbasedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
