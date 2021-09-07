e5974bb946bd10d8f517227e7a394408aa559e8a
jiangwenping <jwpttcg66>
2021-09-07 16:54:56 +0800

    init commit

--- src/test/java/com/snowcattle/game/hbasedemo/HBaseClientTest.java
+++ src/test/java/com/snowcattle/game/hbasedemo/HBaseClientTest.java
@@ -6,12 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.lang.annotation.Target;
-
-import static org.junit.Assert.*;
 
 /**
  *
@@ -20,7 +16,7 @@ import static org.junit.Assert.*;
  *
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class HBaseClientTest {
 
 	private final static String TABLE = "quick-hbase-table";
