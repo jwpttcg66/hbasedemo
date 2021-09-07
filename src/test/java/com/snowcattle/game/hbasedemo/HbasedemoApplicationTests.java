69837a97c10dc4a6277bbf1de3f7a243f7a139cb
jiangwenping <jwpttcg66>
2021-09-07 18:35:15 +0800

    测试服务器产生多个时钟

--- src/test/java/com/snowcattle/game/hbasedemo/HbasedemoApplicationTests.java
+++ src/test/java/com/snowcattle/game/hbasedemo/HbasedemoApplicationTests.java
@@ -3,6 +3,11 @@ package com.snowcattle.game.hbasedemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
 @SpringBootTest
 class HbasedemoApplicationTests {
 
@@ -10,6 +15,40 @@ class HbasedemoApplicationTests {
     void contextLoads() {
 
         System.out.println("hello world");
+        testClock();
+
     }
 
+    public void testClock(){
+        Clock clock = Clock.systemDefaultZone();
+
+        Clock offsetLock = clock.offset(clock, Duration.ofDays(1));
+
+        clock = offsetLock;
+        // get instant of class
+        Instant instant = clock.instant();
+
+        // get ZonedDateTime object from instantObj
+        // to get date time
+        ZonedDateTime time = instant.atZone(clock.getZone());
+
+        // print details of ZonedDateTime
+        System.out.println("ZonedDateTime of class with UTC"
+                + " Time Zone is "
+                + time.toString());
+
+        clock = Clock.systemDefaultZone();
+
+        // get instant of class
+        instant = clock.instant();
+
+        // get ZonedDateTime object from instantObj
+        // to get date time
+        time = instant.atZone(clock.getZone());
+
+        // print details of ZonedDateTime
+        System.out.println("ZonedDateTime of class with UTC"
+                + " Time Zone is "
+                + time.toString());
+    }
 }
